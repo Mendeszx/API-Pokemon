af1b74c231fe80fb0f4f34574eb85937f23144e1
Guilherme Mendes <guilherme_mendes2018@outlook.com>
2022-03-13 23:37:45 -0300

    adicionando CORS

---
+++ src/main/kotlin/com/pokemon/api/configuration/ApiConfiguration.kt
@@ -0,0 +1,20 @@
+package com.pokemon.api.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+
+
+class ApiConfiguration {
+    @Configuration
+    class CorsConfiguration : WebMvcConfigurer {
+        override fun addCorsMappings(registry: CorsRegistry) {
+            registry.addMapping("/**")
+                .allowedOrigins("https://pokedexz.herokuapp.com/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+        }
+    }
+}
