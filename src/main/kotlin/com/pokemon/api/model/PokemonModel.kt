09aaefccb59257d56992bd0f65869bb88dce082b
Guilherme Mendes <guilherme_mendes2018@outlook.com>
2022-03-13 21:06:18 -0300

    update model

--- src/main/kotlin/com/pokemon/api/model/PokemonModel.kt
+++ src/main/kotlin/com/pokemon/api/model/PokemonModel.kt
@@ -1,19 +1,16 @@
 package com.pokemon.api.model
 
 import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity(name = "pokemon")
 data class PokemonModel(
 
-    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
     var id: Long = 0,
     var nome: String = "",
     var altura: Double = 0.0,
     var peso: Double = 0.0,
-    var categoria: String = "",
     var habilidades: String = "",
     var tipo: String = "",
     var imagem: String = ""
