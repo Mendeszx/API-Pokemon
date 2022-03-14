cbeaff035dd923be1e2445ca4cc6bce8daff770a
Guilherme Mendes <guilherme_mendes2018@outlook.com>
2022-03-14 11:59:35 -0300

    Adicionando campos

--- src/main/kotlin/com/pokemon/api/model/PokemonModel.kt
+++ src/main/kotlin/com/pokemon/api/model/PokemonModel.kt
@@ -9,10 +9,13 @@ data class PokemonModel(
     @Id
     var id: Long = 0,
     var nome: String = "",
+    var nomeMiniatura: String = "",
     var altura: Double = 0.0,
     var peso: Double = 0.0,
     var habilidades: String = "",
     var tipo: String = "",
-    var imagem: String = ""
+    var tipoCard: String = "",
+    var imagem: String = "",
+    var imagemMiniatura: String = ""
 
     )
